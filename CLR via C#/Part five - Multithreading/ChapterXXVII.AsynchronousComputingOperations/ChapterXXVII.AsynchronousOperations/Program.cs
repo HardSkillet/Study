32aa615cd6ae993d87ea918f46de9ce30fc912db
HardSkillet <43767465+HardSkillet@users.noreply.github.com>
2022-08-02 23:58:36 +0300

    Add Chapter XXVII

---
+++ CLR via C#/Part five - Multithreading/ChapterXXVII.AsynchronousComputingOperations/ChapterXXVII.AsynchronousOperations/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChapterXXVII.AsynchronousOperations
+{
+    public sealed class Example
+    {
+        private static void ExampleAsync() {
+            Console.WriteLine("Main thread: queuing an asynchronous operation");
+            ThreadPool.QueueUserWorkItem(ComputeBoundOp, 12.5d);
+            Console.WriteLine("Main thread: Doing other work here...");
+            Thread.Sleep(10000);
+            Console.WriteLine("Hit <Enter> to end this program");
+            Console.ReadLine();
+        }
+        private static void ComputeBoundOp(Object state) {
+            Console.WriteLine("In ComputeBoundOp: state={0}", state);
+            Thread.Sleep(1000);
+        }
+        private static void ExampleContext() {
+            CallContext.LogicalSetData("Name", "Nikita");
+
+            ThreadPool.QueueUserWorkItem(
+                state => Console.WriteLine("Name={0}",
+                CallContext.LogicalGetData("Name")));
+            ExecutionContext.SuppressFlow();            //Запрещаем использование контекста
+            ThreadPool.QueueUserWorkItem(
+                state => Console.WriteLine("Name={0}",
+                CallContext.LogicalGetData("Name")));
+            ExecutionContext.RestoreFlow();             //Разрещаем использование контекста
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            //ExampleAsync();
+            ExampleContext();
+        }
+    }
+}
