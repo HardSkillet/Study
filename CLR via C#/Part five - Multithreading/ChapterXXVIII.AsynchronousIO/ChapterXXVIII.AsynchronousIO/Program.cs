0598595629066e63c850e8f1e3575d35f8d08f57
HardSkillet <43767465+HardSkillet@users.noreply.github.com>
2022-08-05 12:40:59 +0300

    Fixes on Chapter XXVII

--- CLR via C#/Part five - Multithreading/ChapterXXVIII.AsynchronousIO/ChapterXXVIII.AsynchronousIO/Program.cs
+++ CLR via C#/Part five - Multithreading/ChapterXXVIII.AsynchronousIO/ChapterXXVIII.AsynchronousIO/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO.Pipes;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace ChapterXXVIII.AsynchronousIO
@@ -63,31 +64,85 @@ namespace ChapterXXVIII.AsynchronousIO
     }
     internal sealed class Type1 { }
     internal sealed class Type2 { }
+    public static class TaskLogger
+    {
+        public enum TaskLogLevel { None, Pending }
+        public static TaskLogLevel LogLevel { get; set; }
+
+        public sealed class TaskLogEntry {
+            public Task Task { get; internal set; }
+            public DateTime LogTime { get; internal set; }
+            public String Tag { get; internal set; }
+            public String CallerMemberName { get; internal set; }
+            public String CallerFilePath { get; internal set; }
+            public Int32 CallerLineNumber { get; internal set; }
+            public override String ToString()
+            {
+                return String.Format("LogTime={0}, Tag={1}, Member={2}, File={3}({4})",
+                  LogTime, Tag ?? "(none)", CallerMemberName, CallerFilePath,
+                  CallerLineNumber);
+            }
+        }
+
+        private static readonly ConcurrentDictionary<Task, TaskLogEntry> s_log =
+            new ConcurrentDictionary<Task, TaskLogEntry>();
+        public static IEnumerable<TaskLogEntry> GetLogEntries() { return s_log.Values; }
+        public static Task<TResult> Log<TResult>(this Task<TResult> task,
+            String tag = null,
+            [CallerMemberName] String callerMemberName = null,
+            [CallerFilePath] String callerFilePath = null,
+            [CallerLineNumber] Int32 callerLineNumber = 1)
+        {
+            return (Task<TResult>)
+                Log((Task)task, tag, callerMemberName, callerFilePath, callerLineNumber);
+        }
+        public static Task Log(this Task task,
+            String tag = null,
+            [CallerMemberName] String callerMemberName = null,
+            [CallerFilePath] String callerFilePath = null,
+            [CallerLineNumber] Int32 callerLineNumber = 1)
+        {
+            if (LogLevel == TaskLogLevel.None) return task;
+            var logEntry = new TaskLogEntry
+            {
+                Task = task,
+                LogTime = DateTime.Now,
+                Tag = tag,
+                CallerMemberName = callerMemberName,
+                CallerFilePath = callerFilePath,
+                CallerLineNumber = callerLineNumber
+            };
+            s_log[task] = logEntry;
+            task.ContinueWith(t =>
+            {
+                TaskLogEntry entry; s_log.TryRemove(t, out entry);
+            }, TaskContinuationOptions.ExecuteSynchronously);
+            return task;
+        }
+        public static async Task Go()
+        {
+#if DEBUG
+            TaskLogger.LogLevel = TaskLogger.TaskLogLevel.Pending;
+#endif
+            var tasks = new List<Task> {
+                Task.Delay(2000).Log("2s op"),
+                Task.Delay(5000).Log("5s op"),
+                Task.Delay(6000).Log("6s op")
+            };
+
+            try {
+                await Task.WhenAll(tasks);
+            }
+            catch (OperationCanceledException) { }
+            foreach (var op in TaskLogger.GetLogEntries().OrderBy(tle => tle.LogTime))
+                Console.WriteLine(op);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            var a = KekAsync();
-            Console.WriteLine("adasdasd");
-            for (int i = 0; i < 100000; i++) { Console.WriteLine("kek"); Thread.Sleep(100); }
-            Console.ReadLine();
-        }
-        public static async Task KekAsync() {
-            for (Int32 i = 0; i < 10; i++)
-            {
-                Console.WriteLine(i);
-                Thread.Sleep(100);
-            }
-            await Task.Run(() => { 
-                Console.WriteLine("Method1Async");
-                Thread.Sleep(1000);
-                Console.WriteLine("Method1Async");
-            });
-            for(Int32 i = 0; i < 10; i++)
-            {
-                Console.WriteLine(i+10);
-                Thread.Sleep(100);
-            }
+            
         }
     }
 }
